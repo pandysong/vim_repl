ee33eb594935948bae337ea7d76ae1d49f6ec42a
pandy song <pandy.song@gmail.com>
2018-09-12 13:57:13 +1000

    Update repl_client.py

--- python/repl_client.py
+++ python/repl_client.py
@@ -1,13 +1,29 @@
 import vim
+import socket
+
+
+def _eval_in_server(lines):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(('127.0.0.1', 9987))
+    for line in lines:
+        s.send((line+'\n').encode())
+        data = s.recv(1024)
+    s.close
 
 
 def eval_repl(filetype, firstline, lastline):
     s, e = int(firstline) - 1, int(lastline)
+    if e-s > 1:
+        out = [':{'] + vim.current.buffer[s:e] + [':}']
+    else:
+        out = vim.current.buffer[s:e]
 
-    str_in = ["", "# Input: "] + vim.current.buffer[s:e+1]
-    str_out = ["# Output", "output line1", "output line2"]
+    _eval_in_server(out)
+
+    # str_in = ["", "# Input: "] + vim.current.buffer[s:e+1]
+    # str_out = ["# Output", "output line1", "output line2"]
     # print("start,end", s, e)
     # print(str_in)
     # print(str_out)
 
-    vim.current.buffer[e+1:e+1] = str_in + str_out
+    #vim.current.buffer[e+1:e+1] = str_in + str_out
